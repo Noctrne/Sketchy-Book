736b291e75c7271118f6eb62dbdafa99db720ad8
Noctrne <dhiipt32@gmail.com>
2026-07-23 12:03:04 +0700

    move repo

---
+++ Signal-Processing/1_plotsignal.py
@@ -0,0 +1,32 @@
+import numpy as np
+from numpy import pi
+import matplotlib.pyplot as plt
+
+class SinSignal:
+    def __init__(self, amplitude: int, freq: int, ):
+        self.xt
+        self.amp = amplitude
+        self.f = freq
+
+    def signal(self):
+        return 
+    def show(self, times: int):
+
+class CosSignal:
+    def __init__(self):
+        pass
+
+
+def main():
+    while True:
+        try:
+            get_signal_type = int(input('Which type of signal need to plot?'))
+        except ValueError:
+            print()
+
+            
+
+
+
+if __name__ == '__main__':
+    main()
