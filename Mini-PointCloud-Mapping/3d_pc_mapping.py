fb3061c9e42fed8987fa69908c28768918726c92
Noctrne <dhiipt32@gmail.com>
2026-07-23 16:33:53 +0700

    add 3d point cloud mapping

---
+++ Mini-PointCloud-Mapping/3d_pc_mapping.py
@@ -0,0 +1,20 @@
+import numpy as np
+from numpy import random
+import matplotlib.pyplot as plt 
+
+class PointCloud:
+    def __init__(self, p: int, l: int, t: int, ):
+        self.p = p
+        self.l = l
+        self.t = t
+
+
+
+    def volumetric_plot
+
+def main():
+    dataPointCloud = PointCloud()
+
+
+if __name__ == '__main__':
+    main()
--- Signal-Processing/1_plotsignal.py
+++ Signal-Processing/1_plotsignal.py
@@ -7,24 +7,41 @@ class SinSignal:
         self.xt
         self.amp = amplitude
         self.f = freq
-
-    def signal(self):
-        return 
-    def show(self, times: int):
+    
+    def plot(self, times: int):
 
 class CosSignal:
-    def __init__(self):
-        pass
+    def __init__(self, amplitude: int, freq: int):
+        self.xt
+        self.amp = amplitude
+        self.f = freq
+
+    def plot(self):
+
 
 
 def main():
     while True:
         try:
-            get_signal_type = int(input('Which type of signal need to plot?'))
+            print('Which type of signal need to plot?\n 1. Single sin\n 2. Single cos\n 3. Mixed signal')
+            get_signal_type = int(input('Type 1/2/3'))
+            if get_signal_type not in [1,2,3]:
+                print('Input between 1 2 or 3')
+            else:
+                break
         except ValueError:
-            print()
-
-            
+            print('Print only number ')
+    match get_signal_type:
+        case 1:
+            '''sin signal'''
+            signal = SinSignal()
+        case 2:
+            '''cos signal'''
+            signal = CosSignal()
+        case 3:
+            '''mixed mechanism signal'''
+            while True:
+                print('how many s')
 
 
 
